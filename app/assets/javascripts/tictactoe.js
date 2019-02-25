c33d9505edf13e927f584934920de59f5ff787cf
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-25 16:17:00 -0500

    attach square listeners

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,3 +1,4 @@
+
 turn = 0 
 
 function player() {
@@ -8,23 +9,36 @@ function updateState() {
 }
 function setMessage() {
   // Accepts a string and adds it to the`div#message` element in the DOM.
+  
 }
 function checkWinner() {
   // Returns`true` if the current board contains any winning combinations(three`X` or`O` tokens in a row, vertically, horizontally, or diagonally).Otherwise, returns`false`.
   // If there is a winning combination on the board, `checkWinner()` should invoke`setMessage()`, passing in the appropriate string based on who won: `'Player X Won!'` or`'Player O Won!'`
 }
-function doTurn() {
+function doTurn(square) {
   // Increments the`turn` variable by`1`.
+  turn++
   // Invokes the`updateState()` function, passing it the element that was clicked.
+  updateState(square)
+  checkWinner()
   // Invokes`checkWinner()` to determine whether the move results in a winning play.
 }
 function attachListeners() {
   // Attaches the appropriate event listeners to the squares of the game board as well as for the`button#save`, `button#previous`, and`button#clear` elements.
   // When a user clicks on a square on the game board, the event listener should invoke`doTurn()` and pass it the element that was clicked.
+  attachSquareListeners()
   // *** NOTE ***: `attachListeners()` _must_ be invoked inside either a`$(document).ready()`(jQuery) or a`window.onload = () => {}`(vanilla JavaScript).Otherwise, a number of the tests will fail(not to mention that your game probably won't function in the browser).
   // When you name your save and previous functions, make sure to call them something like`saveGame()` and`previousGames()`.If you call them`save()` and`previous()` you may run into problems with the test suite.
 }
 
+// ON LOAD
 $(function () {
   mocha.run()
+  attachListeners()
 })
+
+function attachSquareListeners() {
+  $('td').on('click', function(){
+    doTurn(this)
+  })
+}
