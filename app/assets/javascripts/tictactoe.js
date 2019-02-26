81cb595bce6f2700b956a37682845b260cf5007c
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-26 12:08:15 -0500

    implement saveGame()

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,4 +1,3 @@
-let gameID;
 turn = 0;
 shouldDebug = false;
 
@@ -78,19 +77,47 @@ function doTurn(square) {
 
 function attachListeners() {
   // Attaches the appropriate event listeners to the squares of the game board as well as for the`button#save`, `button#previous`, and`button#clear` elements.
-  // When a user clicks on a square on the game board, the event listener should invoke`doTurn()` and pass it the element that was clicked.
   $("td").on("click", function() {
     doTurn(this);
   });
-  // *** NOTE ***: `attachListeners()` _must_ be invoked inside either a`$(document).ready()`(jQuery) or a`window.onload = () => {}`(vanilla JavaScript).Otherwise, a number of the tests will fail(not to mention that your game probably won't function in the browser).
+  $("#save").on("click", function() {
+    saveGame();
+  });
+  $("#previous").on("click", function() {
+    doTurn(this);
+  });
+  $("#clear").on("click", function() {
+    doTurn(this);
+  });
   // When you name your save and previous functions, make sure to call them something like`saveGame()` and`previousGames()`.If you call them`save()` and`previous()` you may run into problems with the test suite.
 }
 
 // ON LOAD
+// *** NOTE ***: `attachListeners()` _must_ be invoked inside either a`$(document).ready()`(jQuery) or a`window.onload = () => {}`(vanilla JavaScript).Otherwise, a number of the tests will fail(not to mention that your game probably won't function in the browser).
 $(function() {
   mocha.run();
   attachListeners();
 });
+
+let gameId;
+
+function saveGame() {
+  let game = { state: currentState() };
+  if (gameId) {
+    // UPDATE GAME
+    let updating = $.ajax({
+      url: `/games/${gameId}`,
+      data: game,
+      type: "PATCH"
+    });
+    // let updating = $.patch(`/games/${gameId}`, game)
+    updating.done(data => (gameId = data.data.id));
+  } else {
+    // CREATE GAME
+    let posting = $.post("/games", game);
+    posting.done(data => (gameId = data.data.id));
+  }
+}
 
 function getOrCreateGameId() {
   $.get("/games/", data => {
